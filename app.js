18c2f250cb3aca62ea59807bd83754dacfca5b92
Hector Romo <hectorommmo@gmail.com>
2022-05-13 22:55:09 -0700

    updated HTML template

--- app.js
+++ app.js
@@ -2,16 +2,6 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./src/page-template');
 
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-// const [name, github] = profileDataArgs;
-
-// fs.writeFile('index.html', generatePage(name, github), err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
-
 const promptUser = () => {
     return inquirer
         .prompt([{
@@ -71,10 +61,10 @@ const promptProject = portfolioData => {
     }
 
     console.log(`
-  =================
-  Add a New Project
-  =================
-  `);
+    =================
+    Add a New Project
+    =================
+    `);
     return inquirer
         .prompt([{
                 type: 'input',
@@ -149,9 +139,9 @@ promptUser()
     .then(portfolioData => {
         const pageHTML = generatePage(portfolioData);
 
-        // fs.writeFile('./index.html', pageHTML, err => {
-        //   if (err) throw new Error(err);
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
 
-        //   console.log('Page created! Check out index.html in this directory to see it!');
-        // });
+            console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
