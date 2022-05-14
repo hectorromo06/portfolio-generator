ac3f666249915926a09b1c170926704847f504ae
Hector Romo <hectorommmo@gmail.com>
2022-05-14 16:23:09 -0700

    modularize page template / style

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
+const generatePage = require('./src/js/page-template');
 
 const promptUser = () => {
     return inquirer
@@ -135,13 +135,20 @@ const promptProject = portfolioData => {
 };
 
 promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw new Error(err);
-
-            console.log('Page created! Check out index.html in this directory to see it!');
-        });
-    });
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
